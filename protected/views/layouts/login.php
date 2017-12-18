528ef8627f841950b5808018bbada84d168751b3
aleksandr.tarasov <a.tarasov1209@gmail.com>
2017-12-18 21:15:18 +0000

    changed some styles

--- protected/views/layouts/login.php
+++ protected/views/layouts/login.php
@@ -20,46 +20,13 @@
         <script src="<?php echo Yii::app()->request->baseUrl; ?>/lib/bootstrap/js/bootstrap.min.js"></script>
         
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/lib/fontawesome/css/font-awesome.min.css">
+        
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/styles.css">
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
 <body>
-    <div class="container-full">
-        <div id="myCarousel" class="carousel slide" data-ride="carousel">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                <li data-target="#myCarousel" data-slide-to="1"></li>
-                <li data-target="#myCarousel" data-slide-to="2"></li>
-            </ol>
-
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner">
-                <div class="item active">
-                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/1.jpg" alt="Los Angeles" style="width:100%;">
-                </div>
-
-                <div class="item">
-                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/2.jpg" alt="Chicago" style="width:100%;">
-                </div>
-
-                <div class="item">
-                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/3.jpg" alt="New york" style="width:100%;">
-                </div>
-            </div>
-
-            <!-- Left and right controls -->
-            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
-    </div>
     
     <div class="container">
         <div class="row">
@@ -83,6 +50,42 @@
                     <button id="logoutBtn" type="button" class="btn btn-primary"><i class="fa fa-sign-out" aria-hidden="true"></i> Logout</button>
                 </div>
             <?php } ?>
+        </div>
+    </div>
+    
+    <div class="container top30">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+                <div class="item active">
+                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/1.jpg" alt="Los Angeles" style="width:100%;">
+                </div>
+
+                <div class="item">
+                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/1.jpg" alt="Chicago" style="width:100%;">
+                </div>
+
+                <div class="item">
+                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/1.jpg" alt="New york" style="width:100%;">
+                </div>
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
     </div>
 
