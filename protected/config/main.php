ed524658e88adc3a3769a9641474c6ed735e4b77
aleksandr.tarasov <a.tarasov1209@gmail.com>
2018-01-10 00:01:53 +0000

    ### implemented Yii logiing for debugging

--- protected/config/main.php
+++ protected/config/main.php
@@ -59,21 +59,23 @@ return array(
 			'errorAction'=>YII_DEBUG ? null : 'site/error',
 		),
 
-		'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
-				array(
-					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
-				),
-				// uncomment the following to show log messages on web pages
-				/*
-				array(
-					'class'=>'CWebLogRoute',
-				),
-				*/
-			),
-		),
+		'log' => array(
+                    'class' => 'CLogRouter',
+                    'routes' => array(
+                        array(
+                            'class' => 'CFileLogRoute',
+                            'levels' => 'trace, info, error, warning, vardump',
+                        ),
+                        // uncomment the following to show log messages on web pages
+//                        array(
+//                            'class' => 'CWebLogRoute',
+//                            'enabled' => YII_DEBUG,
+//                            'levels' => 'error, warning, trace, notice',
+//                            'categories' => 'application',
+//                            'showInFireBug' => false,
+//                        ),
+                    ),
+                ),
 
 	),
 
