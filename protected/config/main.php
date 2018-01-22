d73ae9c4f888871ec41408f03cf2547d3ed9acdb
aleksandr.tarasov <a.tarasov1209@gmail.com>
2018-01-22 02:29:13 +0000

    #### logging for debugging improved

--- protected/config/main.php
+++ protected/config/main.php
@@ -45,43 +45,51 @@ return array(
 			'allowAutoLogin'=>true,
 		),
 
-		// uncomment the following to enable URLs in path-format
-		
-		'urlManager'=>array(
-			'urlFormat'=>'path',
-			'rules'=>array(
-				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
-				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-			),
-		),
-		
+            // uncomment the following to enable URLs in path-format
 
-		// database settings are configured in database.php
-		'db'=>require(dirname(__FILE__).'/database.php'),
+            'urlManager'=>array(
+                    'urlFormat'=>'path',
+                    'rules'=>array(
+                            '<controller:\w+>/<id:\d+>'=>'<controller>/view',
+                            '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+                            '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+                    ),
+            ),
 
-		'errorHandler'=>array(
-			// use 'site/error' action to display errors
-			'errorAction'=>YII_DEBUG ? null : 'site/error',
-		),
 
-		'log' => array(
-                    'class' => 'CLogRouter',
-                    'routes' => array(
-                        array(
-                            'class' => 'CFileLogRoute',
-                            'levels' => 'trace, info, error, warning, vardump',
-                        ),
-                        // uncomment the following to show log messages on web pages
-//                        array(
-//                            'class' => 'CWebLogRoute',
-//                            'enabled' => YII_DEBUG,
-//                            'levels' => 'error, warning, trace, notice',
-//                            'categories' => 'application',
-//                            'showInFireBug' => false,
-//                        ),
+            // database settings are configured in database.php
+            'db'=>require(dirname(__FILE__).'/database.php'),
+
+            'errorHandler'=>array(
+                    // use 'site/error' action to display errors
+                    'errorAction'=>YII_DEBUG ? null : 'site/error',
+            ),
+
+            'log' => array(
+                'class' => 'CLogRouter',
+                'routes' => array(
+                    array(
+                        'class' => 'CFileLogRoute',
+                        'levels' => 'error, warning, trace',
+                    ),
+                    array(
+                        'class' => 'CWebLogRoute', 'enabled' => YII_DEBUG,
+                        'levels' => 'error, warning, trace',
+                        'categories' => 'http.*',
+                        'showInFireBug' => true,
+                    ),
+                    array(
+                        'class' => 'CFileLogRoute', 'enabled' => YII_DEBUG,
+                        'logFile' => 'http.log',
+                        'categories' => 'http.*',
+                    ),
+                    array(
+                        'class' => 'CFileLogRoute', 'enabled' => YII_DEBUG,
+                        'logFile' => 'http.cookies.log',
+                        'categories' => 'http.cookies',
                     ),
                 ),
+            ),
 
 	),
 
