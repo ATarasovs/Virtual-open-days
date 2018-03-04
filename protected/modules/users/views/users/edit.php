d6ca47fbbc0bc63dd119a2f5e4980b9e32924a1e
aleksandr.tarasov <a.tarasov1209@gmail.com>
2018-03-04 18:58:29 +0000

    users edit validation

--- protected/modules/users/views/users/edit.php
+++ protected/modules/users/views/users/edit.php
@@ -36,41 +36,41 @@
 <div class="row">
     <div class="col-sm-4">
         <span class="left"><?php echo $form->labelEx($model,'username', array('class'=>'form-signin-heading')); ?></span>
-        <?php echo $form->textField($model,'username',array('id' => 'username', 'class'=>'form-control', 'placeholder' => 'Username')); ?>
+        <?php echo $form->textField($model,'username',array('id' => 'username', 'class'=>'form-control', 'placeholder' => 'Username', 'data-validation' => 'required', 'data-validation-error-msg' => 'The username must not be empty')); ?>
     </div>
     
     <div class="col-sm-4">
         <span class="left"><?php echo $form->labelEx($model,'firstName', array('class'=>'form-signin-heading')); ?></span>
-        <?php echo $form->textField($model,'firstName',array('id' => 'firstName', 'class'=>'form-control', 'placeholder' => 'First Name')); ?>
+        <?php echo $form->textField($model,'firstName',array('id' => 'firstName', 'class'=>'form-control', 'placeholder' => 'First Name', 'data-validation' => 'required', 'data-validation-error-msg' => 'The first name must not be empty')); ?>
     </div>
     
     <div class="col-sm-4">
         <span class="left"><?php echo $form->labelEx($model,'lastName', array('class'=>'form-signin-heading')); ?></span>
-        <?php echo $form->textField($model,'lastName',array('id' => 'lastName', 'class'=>'form-control', 'placeholder' => 'Last Name')); ?>
+        <?php echo $form->textField($model,'lastName',array('id' => 'lastName', 'class'=>'form-control', 'placeholder' => 'Last Name', 'data-validation' => 'required', 'data-validation-error-msg' => 'The last name must not be empty')); ?>
     </div>
 </div>
 
 <div class="row">
     <div class="col-sm-4">
         <span class="left"><?php echo $form->labelEx($model,'email', array('class'=>'form-signin-heading')); ?></span>
-        <?php echo $form->textField($model,'email',array('id' => 'email', 'class'=>'form-control', 'placeholder' => 'Email')); ?>
+        <?php echo $form->textField($model,'email',array('id' => 'email', 'class'=>'form-control', 'placeholder' => 'Email', 'data-validation' => 'email')); ?>
     </div>
     
     <div class="col-sm-4">
         <span class="left"><?php echo $form->labelEx($model,'phone', array('class'=>'form-signin-heading')); ?></span>
-        <?php echo $form->textField($model,'phone',array('id' => 'phone', 'class'=>'form-control', 'placeholder' => 'Phone')); ?>
+        <?php echo $form->textField($model,'phone',array('id' => 'phone', 'class'=>'form-control', 'placeholder' => 'Phone', 'data-validation' => 'number')); ?>
     </div>
     
     <div class="col-sm-4">
         <span class="left"><?php echo $form->labelEx($model,'country', array('class'=>'form-signin-heading')); ?></span>
-        <?php echo $form->textField($model,'country',array('id' => 'country', 'class'=>'form-control', 'placeholder' => 'Country')); ?>
+        <?php echo $form->textField($model,'country',array('id' => 'country', 'class'=>'form-control', 'placeholder' => 'Country', 'data-validation' => 'country', 'data-validation-error-msg' => 'The country is incorrect')); ?>
     </div>
 </div>
 
 <div class="row">
     <div class="col-sm-4">
         <span class="left"><?php echo $form->labelEx($model,'city', array('class'=>'form-signin-heading')); ?></span>
-        <?php echo $form->textField($model,'city',array('id' => 'city', 'class'=>'form-control', 'placeholder' => 'City')); ?>
+        <?php echo $form->textField($model,'city',array('id' => 'city', 'class'=>'form-control', 'placeholder' => 'City', 'data-validation' => 'required', 'data-validation-error-msg' => 'The city must not be empty')); ?>
     </div>
     
     <div class="col-sm-4">
@@ -80,14 +80,19 @@
     
     <div class="col-sm-4">
         <span class="left"><?php echo $form->labelEx($model,'birthday', array('class'=>'form-signin-heading')); ?></span>
-        <?php echo $form->textField($model,'birthday',array('id' => 'birthday', 'class'=>'form-control', 'placeholder' => 'Birthday')); ?>
+        <?php echo $form->textField($model,'birthday',array('id' => 'birthday', 'class'=>'form-control', 'placeholder' => 'Birthday', 'data-validation' => 'birthdate', 'data-validation-format' => 'dd/mm/yyyy', 'data-validation-error-msg' => 'The birthday should be in the format "dd/mm/yyyy"')); ?>
     </div>
 </div>
 
 <div class="row">
     <div class="col-sm-4">
         <span class="left"><?php echo $form->labelEx($model,'password', array('class'=>'form-signin-heading')); ?></span>
-        <?php echo $form->passwordField($model,'password',array('id' => 'password', 'class'=>'form-control', 'placeholder' => 'Password')); ?>
+        <?php echo $form->passwordField($model,'password',array('id' => 'password', 'name' => 'pass_confirmation', 'class'=>'form-control', 'placeholder' => 'Password', 'data-validation' => 'length', 'data-validation-length' => '7-20', 'data-validation-error-msg' => 'The password must be  between 7-20 characters')); ?>
+    </div>
+    
+    <div class="col-sm-4">
+        <label>Confirm password</label>
+        <input class="form-control" placeholder="Confirm password" type="password" name="pass" data-validation="confirmation" data-validation-error-msg = "Your password and password confiramtion must be same">
     </div>
 </div>
 
@@ -95,16 +100,29 @@
 
 <div class="row">
     <div class="col-sm-4">
-        <span class="left"><label class="form-signin-heading">Current image</label></span>
+        <span class="left"><label class="form-signin-heading">Current image (Ratio 1:1)</label></span>
         <?php if ($model->userImage != "") { ?>  
-            <div class="left"><img class="uploadImg" src="/vod/images/users/<?php echo $model->userImage ?>" alt=""></div>
+            <div class="left"><img class="uploadImg" src="/vod/images/users/<?php echo $model->userImage ?>" alt="" width="200" height="200"></div>
         <?php } else {?>
-            <div class="left"><img class="uploadImg" src="/vod/images/no-image.png" alt=""></div>
+            <div class="left"><img class="uploadImg" src="/vod/images/no-image.png" alt="" width="200" height="200"></div>
         <?php } ?>
-    </div>
-    <div class="col-sm-4">
-        <span class="left"><?php echo $form->labelEx($model,'image', array('class'=>'form-signin-heading')); ?></span>
-        <?php echo $form->fileField($model, 'image'); ?>
+    
+        <div class="top15"></div>
+        
+        <div class="input-group image-preview">
+            <input type="text" class="form-control image-preview-filename" disabled="disabled">
+            <span class="input-group-btn">
+                <div class="btn btn-default image-preview-input">
+                    <span class="glyphicon glyphicon-folder-open"></span>
+                    <?php if ($model->userImage == "") { ?>  
+                        <span class="image-preview-input-title">Browse</span>
+                    <?php } else {?>
+                        <span class="image-preview-input-title">Change</span>
+                    <?php } ?>
+                    <?php echo $form->fileField($model, 'image',array('id' => 'fileImage', 'data-validation' => 'mime', 'data-validation-allowing' => 'jpg, png, gif, jpeg, bmp, tiff', 'data-validation-error-msg' => 'Only files of type jpg, png, gif, jpeg, bmp, tiff are allowed')); ?>
+                </div>
+            </span>
+        </div>
     </div>
 </div>
 
@@ -114,7 +132,7 @@
     <div class="col-md-12">
         <?php
             echo CHtml::htmlButton('<i class="ace-icon fa fa-check"></i> Save', array(
-                'class' => 'btn btn-success btn-sm operationBtn',
+                'class' => 'btn btn-success btn-sm operationBtn submitBtn',
                 'encode' => false,
                 'type' => 'submit',
                 'id' => 'sendBtn')
@@ -133,12 +151,53 @@
     $(document).ready(function() {
         $(".adminLi").addClass("active");
         $("#password").val("");
+        
         initButtons();
+        imagePreview();
+        
+        $(function() {
+            $('#birthday').datepicker({dateFormat: 'dd/mm/yy'});
+        });  
     });
     
     function initButtons() {
         $( ".backBtn" ).click(function() {
             location.href = userAdminReqUrl;
+        });
+        
+        $( ".submitBtn" ).click(function() {
+            $.validate({
+                modules : 'security, date, location, file',
+
+                onModulesLoaded : function() {
+                    $('#country').suggestCountry();
+                }
+            });
+        });
+    }
+    
+    function imagePreview() {
+        $(function() {
+            // Create the close button
+            var closebtn = $('<button/>', {
+                type:"button",
+                text: 'x',
+                id: 'close-preview',
+                style: 'font-size: initial;',
+            });
+            closebtn.attr("class","close pull-right");
+            
+            // Create the preview image
+            $(".image-preview-input input:file").change(function (){   
+                var file = this.files[0];
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    $(".image-preview-input-title").text("Change");
+                    $(".image-preview-filename").val(file.name); 
+                    $('.uploadImg').attr('src', e.target.result);
+                }        
+                reader.readAsDataURL(file);
+            });
         });
     }
     
