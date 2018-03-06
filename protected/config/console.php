4cecf5eb49c355830571d7852fc8a63f86c9463c
aleksandr.tarasov <a.tarasov1209@gmail.com>
2018-03-06 00:13:25 +0000

    changed the name of applicaiton in config/console and config/main

--- protected/config/console.php
+++ protected/config/console.php
@@ -4,7 +4,7 @@
 // Any writable CConsoleApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Console Application',
+	'name'=>'Virtual Open Days',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
