2f4e99f27f4340ad173f601c9cbf01e71093d7ef
aleksandr.tarasov <a.tarasov1209@gmail.com>
2018-03-07 17:39:08 +0000

    added some changes to analitycs view page

--- protected/modules/analytics/views/analytics/view.php
+++ protected/modules/analytics/views/analytics/view.php
@@ -1,9 +1,39 @@
+<?php 
+    $this->widget('application.components.BreadCrumb', array(
+        'crumbs' => array(
+            array('name' => 'Home', 'url' => array('/site/home')),
+            array('name' => 'Analytics'),
+        ),
+    )); 
+?>
+
+<!--*************************************************************-->
 <div class="row">
-    <?php foreach ($charts as $chart) { ?>
-        <div class="col-md-6">
-            <canvas id="<?php echo $chart->id ?>" width="400" height="400"></canvas>
+    <div class="col-xs-12">
+        <div id="infoMessage">
+            <?php
+                foreach (Yii::app()->user->getFlashes() as $key => $message) {
+                    if ($key == 'notice') {
+                        $key = 'warning';
+                    }
+                    echo '<div class="alert alert-' . $key . ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                    ' . $message . "</div>\n";
+                }
+            ?>
         </div>
-    <?php } ?>
+    </div>
+</div>
+<!--*************************************************************-->
+
+
+<div class="container">  
+    <div class="row">
+        <?php foreach ($charts as $chart) { ?>
+            <div class="col-md-6">
+                <canvas id="<?php echo $chart->id ?>" width="400" height="400"></canvas>
+            </div>
+        <?php } ?>
+    </div>
 </div>
 
 <script>
