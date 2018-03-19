7c291a45b59725be7074c33367f142e60dfa7915
aleksandr.tarasov <a.tarasov1209@gmail.com>
2018-03-19 19:49:38 +0000

    implemented panorama view

--- protected/modules/media/views/media/panorama-view.php
+++ protected/modules/media/views/media/panorama-view.php
@@ -2,5 +2,14 @@
     $locationName = preg_replace("/[^a-zA-Z0-9]+/", "", $location->locationName);
     foreach ($photos as $photo) {
 ?>      
-    <img src="/vod/images/media/panorama/<?php echo $locationName ?>/<?php echo $photo->mediaPath ?>">
+    <div id="panorama"></div>
+    <!--<img src="/vod/images/media/panorama/<?php // echo $locationName ?>/<?php // echo $photo->mediaPath ?>">-->
 <?php } ?>
+
+<script>
+    pannellum.viewer('panorama', {
+        "type": "equirectangular",
+        "panorama": "/vod/images/media/panorama/<?php echo $locationName ?>/<?php echo $photo->mediaPath ?>",
+        "autoLoad": true
+    });
+</script>
