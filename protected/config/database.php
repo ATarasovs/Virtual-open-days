c7eeb8dc286c406091c8a1b361e1e7fe5531cb40
COMPUTING\aleksandrstarasovs <atarasovs@dundee.ac.uk>
2018-03-20 13:09:05 +0000

    ### changed database configuration to computing server

--- protected/config/database.php
+++ protected/config/database.php
@@ -2,11 +2,16 @@
 
 // This is the database connection configuration.
  return array(
- 	'connectionString'=>'mysql:host=localhost;dbname=virtual_open_days',//override my_database for you actual db
- //	 uncomment the following lines to use a MySQL database
- 	'emulatePrepare' => true,
- 	'username' => 'root',
- 	'password' => '',
+ //	'connectionString'=>'mysql:host=localhost;dbname=virtual_open_days',//override my_database for you actual db
+ //	'emulatePrepare' => true,
+ //	'username' => 'root',
+ //	'password' => '',
+ //	'charset' => 'utf8',
+ 
+	'connectionString'=>'mysql:host=silva.computing.dundee.ac.uk;dbname=vodaydb',//override my_database for you actual db
+	'emulatePrepare' => true,
+ 	'username' => 'voday',
+ 	'password' => '8473.vod.3748',
  	'charset' => 'utf8',
  );
 // 
